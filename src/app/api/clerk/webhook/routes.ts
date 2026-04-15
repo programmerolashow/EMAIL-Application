1259e798e9ced3788037cec3803a270875c389fd
programmerolshow <olanrewajuillias@gmail.com>
2026-04-16 00:57:56 +0100

    Updated

--- src/app/api/clerk/webhook/routes.ts
+++ src/app/api/clerk/webhook/routes.ts
@@ -1,9 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { db } from "@/server/db";
 import { type NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { data } = await req.json();
   console.log("Recieved webhook with data:", data)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unused-vars
+  const emailAddress = data.email_addresses[0].email_address
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unused-vars
+  const firstName = data.first_name
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars
+  const lastname = data.last_name
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const imageUrl = data.image_url
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const id = data.id
 
-  return new Response(`Received webhook with data: { status: 200, message: "Success"}`);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  await db.user.create({
+    data: {
+      id: id,
+      email: emailAddress,
+      firstname: firstName,
+      lastname: lastname,
+      imageURL: imageUrl,
+      time: new Date(),
+    }
+  })
+
+  return new Response(`Received webhook with data: { status: 200 }`);
 };
